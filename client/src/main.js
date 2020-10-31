e9724c3d6a17a6b0b3b58df4dc374c8d38d40637
teresa1030 <jingjing10301030@gmail.com>
2020-10-31 21:37:50 +0800

    登入console 報錯解決

--- client/src/main.js
+++ client/src/main.js
@@ -72,6 +72,7 @@ if (localStorage.getItem('token')) {
   userID = loginData.id;
   axios.get('https://hotelapi.im.nuk.edu.tw/api/account/' + userID).then((response) => {
     userAccountDetail = response.data;
+    logout.employeeNumber = userAccountDetail.employeeNumber;
   }).catch((error) => {
     console.log(error);
   })
@@ -83,13 +84,14 @@ router.beforeEach((to, from, next) => {
   if (localStorage.getItem('token')) {
     //判斷當前時間與登入時間差異
     var currentTime = new Date().getTime();
-    if ((currentTime - loginData.time) > 7200000) {
+    if(loginData.time== null){
+      loginData.time = 0;
+    }
+    if ((currentTime - loginData.time) > 7200000) {   
       localStorage.removeItem('token');
       let record = 'logout';
       let company = companyName;
-      logout.employeeNumber = userAccountDetail.employeeNumber;
       logout.logoutTime = dateTime.recordDate() + ' ' + dateTime.recordTime();
-      console.log(logout);
       axios.put('https://hotelapi.im.nuk.edu.tw/api/history/' + company + '/' + record, logout)
         .then((response) => {
           console.log(response);
